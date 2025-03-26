2c8f4dfe820f9ddef7cea0fa362a867d61caf9ef
Cameronstorer <cameronstorer0@gmail.com>
2025-03-25 19:28:18 -0500

    transferred old code

---
+++ scripts/addToolbar.js
@@ -0,0 +1,13 @@
+// This script adds the toolbar button functionality
+
+// javascript to add button functionality
+    // define possible button id's:
+    let buttonNames = ["about", "blog", "contact", "donate"];
+    // take action
+    document.addEventListener("click", function(event) {
+        if (event.target.tagName === "BUTTON") { //Ensure it's a button
+            if (buttonNames.includes(event.target.id)){ // Figure out which button
+                window.parent.location.href = "https://cameronstorer.github.io/Camerons-Tech-Journal/" + event.target.id;
+            }
+        }
+    });
